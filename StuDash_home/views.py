fc1084dc5e7be4350c207a80474b72d8fc8e960a
Noa R <noa.ramon01@gmail.com>
2020-11-13 10:46:50 +0200

    creating a view to display our website

--- StuDash_home/views.py
+++ StuDash_home/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
+def index(request):
+    return render(request, 'StuDash/index.html')
+
 # Create your views here.
--- urls.py
+++ urls.py
@@ -18,4 +18,11 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', view.index, name='index')   
 ]
+
+# Use static() to add url mapping to serve static files during development (only)
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
