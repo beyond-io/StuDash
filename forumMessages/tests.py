d4c8b9320ba3535e8ea2940025674ae342163f13
Gal-f <galfroind@gmail.com>
2021-01-05 20:24:08 +0200

    Category tests
    Adding tests for methods in the Category class

--- forumMessages/tests.py
+++ forumMessages/tests.py
@@ -1,2 +1,21 @@
-# from django.test import TestCase
-# Remove comment when implementing tests
+import pytest
+from django.test import TestCase
+from forumMessages.models import Message, Category
+from django.db.models.query import QuerySet
+from django.contrib.auth.models import User
+from grades.models import Course
+
+@pytest.mark.django_db
+class TestMessages:
+
+    def test_addCategory(cls):
+        Category.addCategory("TestCat")
+        testCat = Category.objects.get(name="TestCat")
+        assert testCat.name == "TestCat"
+
+    def test_getCategories(cls):
+        allCat = Category.getCategories()
+        assert isinstance(allCat, QuerySet) # Check if the list of categories is OK after adding a test category
+        assert all(isinstance(cat, Category) for cat in Category) # Check if all the instances in Category table are of type Category
+        Message.objects.get(name="TestCat").delete() # Delete the test Category after all tests passed
+        # No method for category deletion exists (no scenario for deleting a category, by design), so this is done manually in the above line
