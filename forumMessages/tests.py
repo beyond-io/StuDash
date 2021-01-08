ba7bf29df7b20c8db4fb0f5c49dce47344dcfaa8
Gal-f <galfroind@gmail.com>
2021-01-08 03:03:12 +0200

    Adding a method for creating category
    And moving the method for adding an existing category to a message
    down to the Message class methods.

--- forumMessages/tests.py
+++ forumMessages/tests.py
@@ -1,9 +1,7 @@
 import pytest
-from django.test import TestCase
 from forumMessages.models import Message, Category
 from django.db.models.query import QuerySet
-from django.contrib.auth.models import User
-from grades.models import Course
+
 
 @pytest.mark.django_db
 class TestMessages:
@@ -15,7 +13,9 @@ class TestMessages:
 
     def test_getCategories(cls):
         allCat = Category.getCategories()
-        assert isinstance(allCat, QuerySet) # Check if the list of categories is OK after adding a test category
-        assert all(isinstance(cat, Category) for cat in Category) # Check if all the instances in Category table are of type Category
-        Message.objects.get(name="TestCat").delete() # Delete the test Category after all tests passed
-        # No method for category deletion exists (no scenario for deleting a category, by design), so this is done manually in the above line
+        assert isinstance(allCat, QuerySet)  # Check if the list of categories is OK after adding a test category
+        assert all(isinstance(cat, Category) for cat in Category)  # Check if all the instances in Category table
+        # are of type Category
+        Message.objects.get(name="TestCat").delete()  # Delete the test Category after all tests passed
+        # No method for category deletion exists (no scenario for deleting a category, by design),
+        # so this is done manually in the above line
